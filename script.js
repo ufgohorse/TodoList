a11dbd9647782459605b4aba5829203541dabad3
leonardo-costa <leonardogt2308@gmail.com>
2019-04-20 20:21:32 -0300

    separacao javascript em funcoes + limpar campo

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-var id =0
-var checkbox = []
-function addItem(){
-    var input = document.querySelector("#item").value
-    if(input!=""){
-    var pai = document.querySelector("#taskList")
-    var item = `<li class="t${id} list-group-item" id="taskList">${input}</li>`;
-    pai.insertAdjacentHTML('beforeend',item);
-    id++
-    }else{
-        alert("null")
-    } 
+var id = 0;
+const listaTodos = [];
+const input = document.querySelector("#item");
 
+//funcao executada ao clicar no botao
+function adicionarTodo() {
+  verificarInputVazio();
+  adicionarItemLista();
+  limparInput();
+}
+
+//verifica se o input está preenchido
+function verificarInputVazio() {
+  if (
+    input.value == undefined ||
+    input.value == null ||
+    input.value.length < 1
+  ) {
+    return alert("Ops, você não descreveu a tarefa!");
+  }
+}
+
+//adiciona o item na lista tanto no html, quanto no array de items da lista
+function adicionarItemLista() {
+  //pega o elemento no html onde a lista será mostrada
+  let lista = document.querySelector("#taskList");
+  //'constroi' o elemento com o valor do input
+  let itemLista = `<li class="t${id} list-group-item" id="taskList">${
+    input.value
+  }</li>`;
+  lista.insertAdjacentHTML("beforeend", itemLista);
+  //atualiza o id do item da lista
+  id++;
+  //adiciona item no array
+  listaTodos.push(input.value);
+}
+
+//limpa o input após adicionarmos um item
+function limparInput() {
+  return (input.value = null);
 }
