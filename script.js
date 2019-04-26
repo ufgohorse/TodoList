b00d9f810ca9644b5913f1b1da2107576fec4e39
Kallebe18 <kallebegomes18@gmail.com>
2019-04-26 20:49:50 -0300

    botao do checkbox

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 var id = 0;
 var empty = 1;
 const listaTodos = [];
+const listaChecks = [];
 const input = document.querySelector("#item");
+
+//funcao executada ao dar checklist
+function check(id){
+    var check = document.getElementsByClassName("c"+id)[0];
+  if(listaChecks[id] == 0){
+    check.style.backgroundColor = "green";
+    listaChecks[id] = 1;
+  } else {
+    check.style.backgroundColor = "white";
+    listaChecks[id] = 0;
+  }
+}
+
+//funcao executada ao dar enter com input ativo
+function keyEvent(){
+  key = event.keycode || event.which;
+  if(key == 13){
+    adicionarTodo();
+  }
+}
 
 //funcao executada ao clicar no botao
 function adicionarTodo() {
@@ -31,10 +52,20 @@ function adicionarItemLista() {
   //pega o elemento no html onde a lista será mostrada
   let lista = document.querySelector("#taskList");
   //'constroi' o elemento com o valor do input
-  let itemLista = `<li class="t${id} list-group-item" id="task">${
-    input.value
-  }</li>`;
-  lista.insertAdjacentHTML("beforeend", itemLista);
+  let itemLista = document.createElement("li");
+  let checkBox = document.createElement("button");
+
+  itemLista.setAttribute("class", "t"+id+" list-group-item");
+  itemLista.setAttribute("id", "task");
+  itemLista.innerHTML = input.value;
+  lista.appendChild(itemLista);
+
+  checkBox.setAttribute("class", "c"+id+" list-group-item");
+  checkBox.setAttribute("id", "cheka");
+  checkBox.setAttribute("onclick", "check("+id+")");
+  listaChecks.push(0);
+  lista.appendChild(checkBox);
+
   //atualiza o id do item da lista
   id++;
   //adiciona item no array
